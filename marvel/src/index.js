116e7855adf4f8ef68333a44061694371e3acbe5
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-02-01 10:39:16 +0300

    Генерация случайного персонажа

--- marvel/src/index.js
+++ marvel/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
+import MarvelService from './services/MarvelService';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>
+  </React.StrictMode>
 );
-
