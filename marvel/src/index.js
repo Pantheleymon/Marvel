6495c9f381977ba7fad094652ec2beac24f2f17d
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-02-02 10:09:02 +0300

    Неиспользуемый импорт

--- marvel/src/index.js
+++ marvel/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
-import MarvelService from './services/MarvelService';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
